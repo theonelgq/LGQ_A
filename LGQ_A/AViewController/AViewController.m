02c03b46943c54174b9e32fbbb9c8cced5c272b0
theonelgq <you@example.com>
2020-07-14 10:46:02 +0800

    add avc

---
+++ LGQ_A/AViewController/AViewController.m
@@ -0,0 +1,32 @@
+//
+//  AViewController.m
+//  LGQ_A
+//
+//  Created by 刘国庆 on 2020/7/14.
+//  Copyright © 2020 刘国庆. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
